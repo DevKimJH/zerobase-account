f2c4da4b4ec5d82cd4c86e1dd425373d5e7c8dbf
DevKimJH <hg116701@naver.com>
2022-10-29 19:51:36 +0900

    계좌 생성 API

--- src/main/java/com/example/account/domain/AccountUser.java
+++ src/main/java/com/example/account/domain/AccountUser.java
@@ -1,38 +1,29 @@
 package com.example.account.domain;
 
-import com.example.account.type.AccountStatus;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Account {
+public class AccountUser {
     @Id
-    @GeneratedValue // Account Table의 PK를 Id로 지정해주겠다는 의미
+    @GeneratedValue
     private Long id;
 
-    @ManyToOne
-    private AccountUser accountUser;
-    private String accountNumber;
-
-    @Enumerated(EnumType.STRING)
-    private AccountStatus accountStatus;
-
-    private Long balance;
-
-    private LocalDateTime registeredAt;
-    private LocalDateTime unRegisteredAt;
-
+    private String name;
 
     @CreatedDate
     private LocalDateTime createdAt;
--- src/main/java/com/example/account/repository/AccountRepository.java
+++ src/main/java/com/example/account/repository/AccountRepository.java
@@ -1,13 +1,18 @@
 package com.example.account.repository;
 
 import com.example.account.domain.Account;
+import com.example.account.domain.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 // JpaRepository<Repository가 활용하게 될 Entity, Entity PK의 데이터타입>
+    Optional<Account> findFirstByOrderByIdDesc();
 
+    Integer countByAccountUser(AccountUser accountUser);
 }
